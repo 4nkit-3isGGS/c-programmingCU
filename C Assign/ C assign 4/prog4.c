9af9587380714416caca28074db24742eeea1b69
4nkit-3isGGS <codewithankit487@gmail.com>
2025-05-22 05:20:56 +0000

    completed assignment 4

--- C Assign/ C assign 4/prog4.c
+++ C Assign/ C assign 4/prog4.c
@@ -1,2 +1,18 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+
+int pow(int x, int n) {
+    if (n == 0)
+        return 1;
+    else
+        return x * pow(x, n - 1);
+}
+
+int main() {
+    int base, power;
+    printf("Enter base and power: ");
+    scanf("%d %d", &base, &power);
+
+    printf("%d raised to the power %d is %d\n", base, power, pow(base, power));
+
+    return 0;
+}
